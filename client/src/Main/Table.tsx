018f96fef7464bfd8891ab6485913dbd596bd546
Osher <oshernati22@gmail.com>
2022-01-11 00:59:31 +0200

    osher

--- client/src/Main/Table.tsx
+++ client/src/Main/Table.tsx
@@ -10,15 +10,47 @@ import Gender from "../Assets/Icons/Icon awesome-transgender.svg";
 import City from "../Assets/Icons/Icon metro-location.svg";
 import Path from "../Assets/Icons/Path 6.svg";
 import Logo from "../Assets/Icons/fibonatix-logo.svg";
+import A from "../Assets/Avatar/Ellipse 11.png";
+import B from "../Assets/Avatar/Ellipse 10.png";
+import C from "../Assets/Avatar/Ellipse 12.png";
+import D from "../Assets/Avatar/Ellipse 13.png";
+import E from "../Assets/Avatar/Ellipse 14.png";
+import F from "../Assets/Avatar/Ellipse 15.png";
 
 interface TableProps {
   students?: Student[];
   setStudents?: React.Dispatch<React.SetStateAction<Student[] | undefined>>;
 }
+const getAvatar = (str: string) => {
+  if (str == "A") {
+    return A;
+  }
+  if (str == "B") {
+    return B;
+  }
+  if (str == "C") {
+    return C;
+  }
+  if (str == "D") {
+    return D;
+  }
+  if (str == "E") {
+    return E;
+  }
+  if (str == "F") {
+    return F;
+  }
+};
 const columns = [
   {
-    field: "id",
+    field: "img",
     title: <img src={Path}></img>,
+    render: (rowData: any) => (
+      <img
+        src={getAvatar(rowData.img)}
+        style={{ width: 40, borderRadius: "50%" }}
+      />
+    ),
   },
   { field: "name", title: <img src={Book}></img> },
   { field: "gender", title: <img src={Gender}></img> },
@@ -57,6 +89,10 @@ const Table: React.FC<TableProps> = ({ students, setStudents }) => {
       const temp = students.filter(
         (row) => !selectedRows.includes(row)
       ) as Student[];
+
+      for (let [i, el] of temp.entries()) {
+        el.id = (i + 1).toString();
+      }
 
       setStudents(temp);
 
